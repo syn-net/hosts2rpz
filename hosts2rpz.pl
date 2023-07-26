8cf00e383c5607d93863243532455bdf0f7730c6
Jeffrey Carpenter <1329364+i8degrees@users.noreply.github.com>
2023-07-26 04:18:50 -0500

    Initial refactoring

--- hosts2rpz.pl
+++ hosts2rpz.pl
@@ -1,6 +1,7 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
 #
-# https://github.com/f3sty/hosts2rpz
+# 1. https://github.com/f3sty/hosts2rpz (original version)
+# 2. https://github.com/syn-net/hosts2rpz (refactored version)
 #
 use strict;
 use warnings;
@@ -9,35 +10,23 @@ use List::Util qw[min max];
 use LWP::UserAgent;
 use HTTP::Request;
 
-# set $bind_reload_cmd to whatever is appropriate
-# for your system
-my $bind_reload_cmd = '/usr/sbin/rndc reload';
-
 # default file locations for input and output files
 my $in  = '';
-my $out = '/var/lib/bind/rpz.db';
+my $out = '';
 
-# URLs for dns4me.net hosts files API
-my $version_url = 'https://dns4me.net/api/v2/get_hosts_file_version';
-my $hosts_url   = 'https://dns4me.net/api/v2/get_hosts/hosts';
-
-my $uid;
 my $verbose = 0;
 my $help;
-my $force;
 
 # User-Agent string for http(s) requests
-my $script_version = '0.2';
-my $agent = "hosts2rpz/${script_version} (https://github.com/f3sty/hosts2rpz))";
+my $script_version = '0.3';
+my $agent = "hosts2rpz/${script_version} (https://github.com/syn-net/hosts2rpz))";
 
 # process any args the script was passed
 GetOptions(
     'help'     => \$help,
     'verbose+' => \$verbose,
-    'uid=s'    => \$uid,
     'in=s'     => \$in,
     'out=s'    => \$out,
-    'force'    => \$force
 );
 
 if ($help) {
@@ -51,72 +40,29 @@ my $maxlen = 1;    # length of longest parsed hostname
 my $rrformat;
 my ( $h, $a );
 my $serial = time;
-my $hostsversion;
-my $currentversion;
-my $tmpfile       = "/tmp/hosts.tmp.$$";
-my $clean_tmpfile = 0;
+# FIXME(jeff): I have yet to figure out how to insert these two variables
+# within a "perlform" [1].
+# 1. https://perldoc.perl.org/perlform
+# IMPORTANT(jeff): The authoritative hostname must end with a dot (.)
+my $soa_hostname = 'rpz.lan.';
+# IMPORTANT(jeff): The administrative e-mail address must end with a dot (.)
+my $dns_admin_email = 'hostmaster.lan.';
 
 my $ua = LWP::UserAgent->new;
 $ua->agent($agent);
 
-# if a UUID was passed as a script arg, fetch the
-# current hostsfile version to compare with the local file
-if ($uid) {
-    $version_url .= "/${uid}";
-    $hosts_url   .= "/${uid}";
-    my $req = HTTP::Request->new( GET => $version_url );
-    my $response = $ua->request($req);
-    if ( $response->is_success ) {
-        $hostsversion = $response->decoded_content;
-        $verbose && print "version available: $hostsversion\n";
-    }
-    else {
-        die "Something failed fetching the current hosts version\n";
-    }
-
-    # If we're going to update an existing rpz zone file, check to
-    # see if it matches the current hosts version
-    if ( -f $out ) {
-        open DB, $out;
-        while (<DB>) {
-            if ( $_ =~ m/version:(\w*)/ ) {
-                $currentversion = $1;
-                if ( $verbose >= 2 ) {
-                    print "Current version in use: $currentversion\n";
-                }
-            }
-        }
-        close DB;
-    }
-
-    # If --force was specified, update the zone file
-    # regardless of the local version
-    if ($force) {
-        $verbose && print "Forcing re-generation\n";
-    }
-
-    # otherwise only update it if the versions  don't match
-    elsif ( $currentversion eq $hostsversion ) {
-        $verbose && print "Already up to date, exiting.\n";
-        exit;
-    }
-    $req = HTTP::Request->new( GET => $hosts_url );
-    $response = $ua->request($req);
-    if ( $response->is_success ) {
-        open my $tmp, ">${tmpfile}";
-        print $tmp $response->decoded_content;
-        close $tmp;
-    }
-
-    # Use the feshly retrieved file as our --in,
-    # and mark it for cleanup
-    $in            = $tmpfile;
-    $clean_tmpfile = 1;
-}
+my $exit_script_usage = "Usage: $0 --in [infile] --out [outfile]\n\n";
 
 # read the input file
 unless ( -f $in ) {
-    die "Usage: $0 --in [infile]\n\n";
+    print $exit_script_usage;
+    exit 0;
+}
+
+# we must have a file we can output to
+unless ( $out ) {
+    print $exit_script_usage;
+    exit 0;
 }
 
 open my $infile, "<${in}"
@@ -141,7 +87,8 @@ while (<$infile>) {
 $rrformat =
     "format RR = \n" . '@'
   . '<' x $maxlen
-  . '  IN   A    @<<<<<<<<<<<<<<' . "\n" . '$h,$a' . "\n" . ".\n";
+  . '  IN   CNAME   .' . "\n" . '$h,$a' . "\n" . ".\n";
+  #. '  IN   A    @<<<<<<<<<<<<<<' . "\n" . '$h,$a' . "\n" . ".\n";
 eval $rrformat;
 
 open my $outfile, ">${out}" or die "Error writing to $out\n\n";
@@ -157,35 +104,14 @@ foreach $hostname ( sort keys %rec ) {
     &write_rr( $hostname, $rec{$hostname} );
 }
 
-if ($clean_tmpfile) {
-    if ( $in =~ m/tmp/ ) { unlink($in); }
-}
-
 close $outfile;
 select(STDOUT);
-
-if ($bind_reload_cmd) {
-    my $msg = '';
-    my $exitstatus;
-    $verbose && print "Reloading bind\n";
-    open my $reload, "$bind_reload_cmd|"
-      or die "Failed to execute $bind_reload_cmd - $@\n";
-    while (<$reload>) {
-        $msg .= $_;
-    }
-    close $reload;
-    $exitstatus = $?;
-    $verbose && print $msg;
-    exit($?);
-}
 
 sub printhelp {
     print "\n\nhosts2rpz.pl - convert hosts files to rpz zone format\n";
     print "  Usage: hosts2rpz.pl [-iovuh]\n\n";
-    print "   -u | --uid      dns4me user UUID\n";
     print "   -i | --in       input file\n";
     print "   -o | --out      output file (rpz db)\n";
-    print "   -f | --force    force update\n";
     print "   -v | --verbose  increase script verbosity\n";
     print "   -h | --help     You are here\n\n";
 }
@@ -199,16 +125,16 @@ sub write_rr($$) {
 }
 
 # rpz db SOA template
-format HEADER = 
+format HEADER =
 $TTL 60
-@            IN    SOA  localhost. root.localhost.  (
+@            IN    SOA  rpz.lan. hostmaster.lan.  (
 '@'
-            @>>>>>>>>>>>>>>>  ; serial for version:@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-$serial,$hostsversion
-                          3H  ; refresh 
-                          1H  ; retry 
-                          1W  ; expiry 
-                          1H) ; minimum 
+            @>>>>>>>>>>>>>>>  ; serial:@<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+                          $serial,$serial
+                          3H  ; refresh
+                          1H  ; retry
+                          1W  ; expiry
+                          1H) ; minimum
                   IN    NS    localhost.
 
 .
